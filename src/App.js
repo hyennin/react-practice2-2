9e28fa642cab090f42bf0aba05158a845c94d3c5
hyennin <chm050202@gmail.com>
2023-04-18 20:07:44 +0900

    useState 사용 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // let post = '찬실은 복도 많지!';
+  // const [제목1, b1] = useState('찬실은 복도 많지');
+  // const [제목2, b2] = useState('내 서랍 속에 행복');
+  // const [제목3, b3] = useState('컴온컴온');
+
+  const [제목들, 제목변경] = useState(['찬실은 복도 많지', '내 서랍 속에 행복', '컴온컴온']);
+  const [like, likeAdd] = useState(0);
+  console.log(like[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello~
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4 style={{color : 'yellow', fontSize : '22px'}}>힐링영화</h4>
+      </div>
+      <button onClick={() => {
+          let copy1 = [...제목들];
+          copy1[0] = '벌새';
+          copy1[1] = '우리들';
+          copy1[2] = '가족';
+          제목변경(copy1);
+        }}>신규영화</button>
+      <div className='list'>
+        <h4>{제목들[0]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
+        <p>4월 18일</p>
+      </div>
+      <div className='list'>
+        <h4>{제목들[1]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
+        <p>4월 18일</p>
+      </div>
+      <div className='list'>
+        <h4>{제목들[2]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
+        <p>4월 18일</p>
+      </div>
     </div>
   );
 }
