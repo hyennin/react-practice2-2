ddad014645f2d2924d21d57b28a37c869acd202a
hyennin <chm050202@gmail.com>
2023-04-20 19:57:04 +0900

    map 사용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function App() {
+const App = () => {
   // let post = '찬실은 복도 많지!';
   // const [제목1, b1] = useState('찬실은 복도 많지');
   // const [제목2, b2] = useState('내 서랍 속에 행복');
@@ -10,6 +10,7 @@ function App() {
 
   const [제목들, 제목변경] = useState(['찬실은 복도 많지', '내 서랍 속에 행복', '컴온컴온']);
   const [like, likeAdd] = useState(0);
+  const [modal, setModal] = useState(false);
   console.log(like[0]);
   return (
     <div className="App">
@@ -23,19 +24,32 @@ function App() {
           copy1[2] = '가족';
           제목변경(copy1);
         }}>신규영화</button>
-      <div className='list'>
-        <h4>{제목들[0]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
-        <p>4월 18일</p>
-      </div>
-      <div className='list'>
-        <h4>{제목들[1]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
-        <p>4월 18일</p>
-      </div>
-      <div className='list'>
-        <h4>{제목들[2]} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
-        <p>4월 18일</p>
-      </div>
+      <button onClick={() => {
+          let copy2 = [...제목들];
+          copy2.sort();
+          제목변경(copy2);
+        }}>제목정렬</button>
+      {제목들.map((제목) => {
+        return (
+          <div className='list'>
+            <h4 onClick={() => {setModal(!modal)}}>
+              {제목} <span onClick={() => {likeAdd(like + 1)}}>👍</span> {like}</h4>
+            <p>4월 18일</p>
+          </div>
+        );
+      })}
+      { modal === true ? <Modal/> : null }
     </div>
+  );
+}
+
+const Modal = () => {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+  </div>
   );
 }
 
